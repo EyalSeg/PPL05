f3745f1f523e2a020fa6d5e3e373318aa9fce580
Unknown <eyals.code@gmail.com>
2018-06-15 16:23:36 +0300

    Q6a (Prolog)

--- Id1_Id2/q6.pl
+++ Id1_Id2/q6.pl
@@ -12,3 +12,16 @@
 % X = [3];
 % X = [];
 % false
+
+sub([], List).
+sub(Sublist, List) :-
+    append([Head1], Tail1, Sublist),
+    append([Head2], Tail2, List),
+    Head1 = Head2,
+    sub(Tail1, Tail2).
+sub(Sublist, List) :-
+        append([Head2], Tail2, List),
+        sub(Sublist, Tail2).
+    
+
+    
