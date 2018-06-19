961c5d125e3b4180382fb9438aa87eeb751e82c6
Unknown <eyals.code@gmail.com>
2018-06-19 12:21:45 +0300

    Q6.1 (Prolog) bugfix
    fixed the bug causing some results to return more than once

--- Id1_Id2/q6.pl
+++ Id1_Id2/q6.pl
@@ -13,15 +13,16 @@
 % X = [];
 % false
 
-sub([], List).
+sub([], []).
 sub(Sublist, List) :-
     append([Head1], Tail1, Sublist),
     append([Head2], Tail2, List),
     Head1 = Head2,
     sub(Tail1, Tail2).
+
 sub(Sublist, List) :-
-        append([Head2], Tail2, List),
-        sub(Sublist, Tail2).
+    append([_], Tail2, List),
+    sub(Sublist, Tail2).
     
 
-    
+        
